2031fa0f92b79210846865f9b4e78ff8dc2c5f22
Jesse Sandvik <sandvikjesse@gmail.com>
2021-08-22 15:29:26 -0400

    first commit

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,12 @@
-/**
- * List handler for reservation resources
- */
+const reservationsService = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  const data = await reservationsService.list();
+  res.json({ data });
 }
 
 module.exports = {
-  list,
+  list: [asyncErrorBoundary(list)],
 };
